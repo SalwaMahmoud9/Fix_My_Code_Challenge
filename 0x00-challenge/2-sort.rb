c87e1b259a143799952ed434f757d8d98dafcc55
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-02-12 17:44:08 +0200

    salwa 12-2-2024

---
+++ 0x00-challenge/2-sort.rb
@@ -0,0 +1,27 @@
+#  Sort
+
+result = []
+ARGV.each do |arg|
+    # not integer
+    next if arg !~ /^-?[0-9]+$/
+
+    # convert to integer
+    i_arg = arg.to_i
+
+    # insert
+    is_inserted = false
+    i = 0
+    l = result.size
+    while !is_inserted && i < l do
+        if result[i] < i_arg
+            i += 1
+        else
+            result.insert(i, i_arg)
+            is_inserted = true
+            break
+        end
+    end
+    result << i_arg if !is_inserted
+end
+
+puts result
